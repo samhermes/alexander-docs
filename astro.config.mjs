a53cac9689db8935fdd4056422f52fd53a91ae90
Sam Hermes <samhermes@me.com>
2026-06-05 20:50:09 -0500

    Update homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://samhermes.com',
-  base: '/alexander'
+    markdown: {
+        shikiConfig: {
+            theme: 'houston',
+        },
+    },
 });
